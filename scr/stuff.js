90768b97c8e07900cf626d129b18fcefac2ebf5e
JjKio <109250384+JjKio@users.noreply.github.com>
2022-09-12 17:45:06 -0700

    Update stuff.js

--- scr/stuff.js
+++ scr/stuff.js
@@ -100,8 +100,9 @@ function handleSubmit(event) {
   searchCity(city);
 }
 
+debugger;
+
 function searchLocation(position) {
-  debugger;
   let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
 
