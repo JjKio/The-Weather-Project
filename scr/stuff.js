94c07608e27bfc55f433a45fb5fb68856a95a4fe
JjKio <109250384+JjKio@users.noreply.github.com>
2022-09-26 13:40:29 -0700

    Adjusted some of the CSS and fixed the current location button

--- scr/stuff.js
+++ scr/stuff.js
@@ -104,7 +104,7 @@ function searchLocation(position) {
   let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
 
-  axios.get(apiUrl).then(displayWeatherCondition);
+  axios.get(apiUrl).then(showWeather);
 }
 
 function getCurrentLocation(event) {
