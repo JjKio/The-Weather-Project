82abb98ac4c96e4747af24ea0d852d4558e1bdd1
JjKio <109250384+JjKio@users.noreply.github.com>
2022-09-28 18:23:55 -0700

    Added functions to see the forecast for the week.
    Something not working correctly - need to review changes and notes again

--- scr/stuff.js
+++ scr/stuff.js
@@ -46,7 +46,47 @@ function fromatDate(newDate) {
 console.log(fromatDate(new Date()));
 let currDate = document.querySelector("#current-date");
 currDate.innerHTML = fromatDate(new Date());
-//Current date ^^^
+//Current date ^^^''
+
+//  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class = "row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` <div class="col-2">
+            <div class="weather-forecast-date">
+              ${fromatDate(forecastDay.dt)}
+            </div>
+            <img src="" alt="" />
+            <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature-max">
+                ${Math.round(forecastDay.temp.max)}°
+              </span>
+              <span class="weather-forecast-temperature-min">
+                ${Math.round(forecastDay.temp.min)}°
+              </span>
+            </div>
+          </div>
+        `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function tempLink(event) {
   event.preventDefault();
